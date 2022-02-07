62e5da9d7328d4a3c1cfc32aafff2741f167c86e
Tristan Schwichow <tschwichow@gmail.com>
2022-02-06 21:55:36 -0800

    removed words

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.scss';
 import React from 'react';
-import words from './words'
+import words from './cutdownWords'
 
 function App() {
 
   let [password, setPassword ] = React.useState('')
   let [passwordLength, setPasswordLength] = React.useState(4)
 
-  let filteredWords = words.filter( v => v.length < 6 && v.length > 3 && v[v.length - 1] != 's')
+  // let filteredWords = words.filter( v => v.length < 6 && v.length > 3 && v[v.length - 1] != 's')
+  // let log = '[\n'
+  // filteredWords.forEach(v => log += `'${v}',\n`)
 
-  console.log(filteredWords.length)
+  //TODO: keep going through words -- im at index 6400 going u
+
   let generatePassword = (e) => {
     e.preventDefault()
 
     let newPassword = ''
 
     for (let i = 1; i <= passwordLength; i++) {
-      let randomWord = filteredWords[Math.floor(Math.random() * 7000)]
+      let randomWord = words[Math.floor(Math.random() * 6500)]
       newPassword += i === 1 ? randomWord[0].toUpperCase()  + randomWord.slice(1) : randomWord 
       if ( i === passwordLength) {
         newPassword += Math.floor(Math.random() * 100 )
