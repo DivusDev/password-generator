b16c8256e62fb179adcb8d635eb50c1f73b0ee2f
Tristan Schwichow <tschwichow@gmail.com>
2022-02-08 19:10:59 -0800

    styled everything and added functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.scss';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import words from './cutdownWords'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCopy } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
 
-  let [password, setPassword ] = React.useState('')
-  let [passwordLength, setPasswordLength] = React.useState(4)
+  let [password, setPassword ] = useState('')
+  let [passwordLength, setPasswordLength] = useState(4)
+  let [loading, setLoading] = useState(false)
+  let [showCopyMessage, setShowCopyMessage] = useState(false)
 
   // let filteredWords = words.filter( v => v.length < 6 && v.length > 3 && v[v.length - 1] != 's')
   // let log = '[\n'
@@ -29,16 +34,50 @@ function App() {
       }
     }
 
-    setPassword(newPassword)
+    //one at a time
+    if (loading) return
+    
+    //transition for loading
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+      setPassword(newPassword)
+
+    }, 600)
 
   }
 
 
+  let copyToClipboard = () => {
+
+    navigator.clipboard.writeText(password);
+    setShowCopyMessage(true)
+    setTimeout(() => {
+      setShowCopyMessage(false)
+    }, 200)
+  }
+
+
   return (
-    <div className="">
-      <button onClick={generatePassword}>Generate</button>
-      <div> Password: {password}</div>
+    <>
+    <div className='container'>
+      <div className='button'>
+        <button onClick={generatePassword}>Generate</button>
+      </div>
+      <div className={`password `}>
+        <div
+          onClick={copyToClipboard}
+          className={`field ${password.length ? '' : 'hide'} ${loading && 'loading'}`} style={{width: password.length > 0 ? `${password.length + 5}rem` : '30rem'}} 
+        > 
+          {password} <FontAwesomeIcon className={`copy-icon ${password.length ? '' : 'hide'} `} icon={faCopy} /> 
+        </div>
+      </div>
     </div>
+    <div className={`copied ${showCopyMessage ? '' : 'hide'}`}>
+      Copied to Clipboard!
+    </div>
+    </>
+    
   );
 }
 
